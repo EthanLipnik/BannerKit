a1c0a1923386101342e9f293015d1fe80341921b
Ethan Lipnik <ethanlipnik@gmail.com>
2021-03-16 18:32:47 -0400

    Changed some safe areas

--- Sources/BannerKit/BKView.swift
+++ Sources/BannerKit/BKView.swift
@@ -109,7 +109,7 @@ public class BKView: UIView {
             imageView.image = nil
         }
         
-        topConstraint.constant = (self.superview?.safeAreaInsets ?? self.safeAreaInsets).top + 10
+        topConstraint.constant = 0
         
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 2.5, options: [.curveEaseInOut, .allowUserInteraction, .beginFromCurrentState], animations: { [weak self] in
             guard let self = self else { return }
